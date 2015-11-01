3d06d8598725c78180a9418d8d147a21d9a29cd9
cleanu <qjli@ust.hk>
2015-11-02 01:01:49 +0800

    replace dead link still in progress

--- js/main.js
+++ js/main.js
@@ -3,8 +3,15 @@ $(document).ready(function(){
   $("#addImage").click(function(){
     //alert("fuck");
     bootbox.prompt("Please enter the image url", function(result) {
-      if (result != null)
-        images += "<img src=" + result + "><br>";;
+      if (result != null){
+          images += "<img src=" + result + "><br>";
+      }
     });
   });
+
+  $('img').error(function(){
+        $(this).attr('src', 'img/dead.png');
+  });
+
+
 });
